1340b3f48e2fc4e93b9d400b20436fcc7bf8213d
Kundan <kundank191@gmail.com>
2018-08-04 16:47:14 +0530

    feat : design update for details activity

--- app/src/main/java/com/example/android/popularmovies/ui/detail/SimilarMovieAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/SimilarMovieAdapter.java
@@ -1,7 +1,82 @@
 package com.example.android.popularmovies.ui.detail;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.Utils.GlideApp;
+import com.example.android.popularmovies.Utils.NetworkUtils;
+import com.example.android.popularmovies.data.network.MovieResponse;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by Kundan on 02-08-2018.
  */
-public class SimilarMovieAdapter {
+public class SimilarMovieAdapter extends RecyclerView.Adapter<SimilarMovieAdapter.ViewHolder>{
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.movieTitle_tv)
+        TextView movieNameTV;
+        @BindView(R.id.moviePoster_iv)
+        ImageView moviePosterIV;
+        @BindView(R.id.rating_tv)
+        TextView movieRatingsTV;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+
+    private List<MovieResponse> mMovieList;
+    private final Context mContext;
+
+    /**
+     * @param context   of the Activity
+     * @param movieList The list of movie to be displayed
+     */
+    public SimilarMovieAdapter(Context context, List<MovieResponse> movieList) {
+        mContext = context;
+        mMovieList = movieList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+
+        View movieView = layoutInflater.inflate(R.layout.list_similar_movies_row_item,parent,false);
+        return new ViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final MovieResponse movie = mMovieList.get(position);
+
+        holder.movieNameTV.setText(movie.getMovieTitle());
+        holder.movieRatingsTV.setText(String.format("%s ⭐ ", movie.getVoteAverageOutOfFive().toString()));
+        GlideApp.with(mContext)
+                .load(NetworkUtils.getPosterImageURL(movie.getPosterPath()))
+                .placeholder(R.drawable.image_place_holder_poster)
+                .error(R.drawable.broken_image_poster)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(holder.moviePosterIV);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMovieList.size();
+    }
 }
