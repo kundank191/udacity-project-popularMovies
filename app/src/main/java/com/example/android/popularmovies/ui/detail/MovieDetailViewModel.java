8dbe52a00249de8c1181f77ebb952aad9dddd061
Kundan <kundank191@gmail.com>
2018-08-15 17:36:42 +0530

    feat : implement view model for details activity

--- app/src/main/java/com/example/android/popularmovies/ui/detail/MovieDetailViewModel.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieDetailViewModel.java
@@ -1,7 +1,66 @@
 package com.example.android.popularmovies.ui.detail;
 
+import android.arch.lifecycle.ViewModel;
+
+import com.example.android.popularmovies.data.network.MovieCreditsResponse;
+import com.example.android.popularmovies.data.network.MovieResponse;
+import com.example.android.popularmovies.data.network.MovieReviewsResponse;
+import com.example.android.popularmovies.data.network.MovieTrailersResponse;
+
+import java.util.List;
+
 /**
  * Created by Kundan on 15-08-2018.
  */
-public class MovieDetailViewModel {
+public class MovieDetailViewModel extends ViewModel {
+
+    public MovieDetailViewModel(){
+
+    }
+
+    private MovieResponse mMovie = null;
+    private List<MovieTrailersResponse> mTrailersList = null;
+    private List<MovieResponse> mSimilarMovieList = null;
+    private List<MovieCreditsResponse> mCastList = null;
+    private List<MovieReviewsResponse> mReviewsList = null;
+
+    public MovieResponse getMovie() {
+        return mMovie;
+    }
+
+    public void setMovie(MovieResponse mMovie) {
+        this.mMovie = mMovie;
+    }
+
+    public List<MovieTrailersResponse> getTrailersList() {
+        return mTrailersList;
+    }
+
+    public void setTrailersList(List<MovieTrailersResponse> mTrailersList) {
+        this.mTrailersList = mTrailersList;
+    }
+
+    public List<MovieResponse> getSimilarMovieList() {
+        return mSimilarMovieList;
+    }
+
+    public void setSimilarMovieList(List<MovieResponse> mSimilarMovieList) {
+        this.mSimilarMovieList = mSimilarMovieList;
+    }
+
+    public List<MovieCreditsResponse> getCastList() {
+        return mCastList;
+    }
+
+    public void setCastList(List<MovieCreditsResponse> mCastList) {
+        this.mCastList = mCastList;
+    }
+
+    public List<MovieReviewsResponse> getReviewsList() {
+        return mReviewsList;
+    }
+
+    public void setReviewsList(List<MovieReviewsResponse> mReviewsList) {
+        this.mReviewsList = mReviewsList;
+    }
 }
--- app/src/main/java/com/example/android/popularmovies/ui/detail/MovieDetailViewModelFactory.java
+++ app/src/main/java/com/example/android/popularmovies/ui/detail/MovieDetailViewModelFactory.java
@@ -1,7 +1,24 @@
 package com.example.android.popularmovies.ui.detail;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+
+import com.example.android.popularmovies.ui.list.MovieViewModel;
+
 /**
  * Created by Kundan on 15-08-2018.
+ * View Model which will be used by the detail activity
  */
-public class MovieDetailViewModelFactory {
+public class MovieDetailViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+
+    public MovieDetailViewModelFactory(){
+
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new MovieDetailViewModel();
+    }
 }
